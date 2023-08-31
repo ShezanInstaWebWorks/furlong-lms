fe3e3d405520aef9516cbbcd48cf4fdbce77d702
saad710 <shezan@instawebworks.com.au>
2023-08-31 10:42:04 +0600

    lms data

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+
+import styles from '../styles/Home.module.css'
+import { Divider, Typography } from '@material-ui/core'
+import CardCom from './CardCom'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+    <Typography
+        p={2}
+        sx={{ fontsize: "solid", paddingLeft: "30px" }}
+        variant="h6"
+      >
+Courses(3)
+      </Typography>
+      <br/>
+      <Divider />
+      <br/>
+     
+      <CardCom/>
+    </div>
+  )
+}
